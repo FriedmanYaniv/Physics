8a68924b0479010452c0e68bb051fd6f2a37e74b
Yaniv <yanivfriedman88@gmail.com>
2019-03-25 00:50:21 +0200

    fixed parabolic functions

--- CondensedMatter2/Ex1.py
+++ CondensedMatter2/Ex1.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.misc import derivative
 
 
-def parabolic_potential(a0, a1, a2):
-    fn = lambda t: a0 + a1*t + a2*(t**2)
+# def parabolic_potential(a0, a1, a2):
+#     fn = lambda t: a0 + a1*t + a2*(t**2)
+#     return fn
+
+def parabolic_potential(xmin, bias, t=10):
+    fn = lambda x: bias + (x-xmin)**2 + t
     return fn
 
 
@@ -15,10 +19,17 @@ if __name__ == '__main__':
     for phase in range(num_phases):
         print('choose a1 smaller than a2')
         coefs = [float(x) for x in
-                 input('for phase number {}, please type coefficients a0, a1, a2'.format(phase+1)).split()]
-        potentials.append(parabolic_potential(coefs[0], -coefs[1], coefs[2]))
+                 input('for phase number {}, please type coefficients x_min, bias'.format(phase+1)).split()]
+        # potentials.append(parabolic_potential(coefs[0], -coefs[1], coefs[2]))
+        potentials.append(parabolic_potential(coefs[0], coefs[1]))
 
-    f1 = potentials[0]
-    plt.plot(phi_vec, f1(phi_vec))
-    plt.xlim((0, 1))
-    np.argmin(derivative(f1, phi_vec))
+    for ind, f in enumerate(potentials):
+        plt.plot(phi_vec, f(phi_vec), label='f_{}'.format(ind + 1))
+        plt.xlim((0, 1))
+    plt.legend()
+    plt.grid()
+
+
+    # find common tangent by finding phi for which the abs(derivative1 - derivative2) is minimum
+    np.argmin(derivative(f, phi_vec))
+
