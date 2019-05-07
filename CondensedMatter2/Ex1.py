a228624509bb641c7e475ab21742cc035dcd6d17
Yaniv <yanivfriedman88@gmail.com>
2019-05-07 13:59:32 +0300

    first push of dancers scripts for Yoav

--- CondensedMatter2/Ex1.py
+++ CondensedMatter2/Ex1.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import derivative
+from scipy.optimize import fsolve
 
 
-# def parabolic_potential(a0, a1, a2):
-#     fn = lambda t: a0 + a1*t + a2*(t**2)
-#     return fn
+def add_liquid_phase(potentials):
+    minimums = [f[1][1] for f in potentials]
+    liquid_min = min(minimums) - 0.5
+    liquid_coefs = [np.random.rand(), liquid_min]
+    liquid_phase = parabolic_potential(liquid_coefs[0], liquid_coefs[1])
+    potentials.append([liquid_phase, liquid_coefs])
+    return potentials
+
 
 def parabolic_potential(xmin, bias, t=10):
     fn = lambda x: bias + (x-xmin)**2 + t
@@ -13,23 +19,36 @@ def parabolic_potential(xmin, bias, t=10):
 
 
 if __name__ == '__main__':
+    auto_ans = True
     potentials = []
     phi_vec = np.linspace(0, 1, num=1000)
-    num_phases = int(input('How many phases? '))
+    if auto_ans:
+        num_phases = 1
+    else:
+        num_phases = int(input('How many phases? '))
+
     for phase in range(num_phases):
         print('choose a1 smaller than a2')
-        coefs = [float(x) for x in
-                 input('for phase number {}, please type coefficients x_min, bias'.format(phase+1)).split()]
-        # potentials.append(parabolic_potential(coefs[0], -coefs[1], coefs[2]))
-        potentials.append(parabolic_potential(coefs[0], coefs[1]))
+        if auto_ans:
+            coefs = [np.random.rand(), 1]
+        else:
+            coefs = [float(x) for x in
+                     input('for phase number {}, please type coefficients x_min, bias'.format(phase + 1)).split()]
 
+        # potentials.append(parabolic_potential(coefs[0], -coefs[1], coefs[2]))
+        potentials.append([parabolic_potential(coefs[0], coefs[1]), coefs])
+
+    potentials = add_liquid_phase(potentials)
     for ind, f in enumerate(potentials):
-        plt.plot(phi_vec, f(phi_vec), label='f_{}'.format(ind + 1))
+        plt.plot(phi_vec, f[0](phi_vec), label='f_{}'.format(ind + 1))
         plt.xlim((0, 1))
     plt.legend()
     plt.grid()
 
 
     # find common tangent by finding phi for which the abs(derivative1 - derivative2) is minimum
-    np.argmin(derivative(f, phi_vec))
+    T_0 = 0
+    dist = min([f[1][1] for f in potentials[:-1]]) - potentials[-1][1][1]
+    T_c = f[-1]
 
+    np.argmin(abs(derivative(f[0], phi_vec)))
---
+++ Research/dancers.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+
+def calc_new_theta(room, dancer):
+    interaction_r = 1
+    # nn_dancers = find_nn_dancers(dancer.location_x, dancer.location_y, room)
+    nn_dancers = find_nn_dancers_with_blocks(dancer.location_x, dancer.location_y, room)
+    return 0
+
+
+def find_nn_dancers_with_blocks(x, y, room):
+    all_locations = np.array([[dancer.location_x, dancer.location_y] for dancer in room.dancers])
+    ignore_r = 5
+    b_half_r = 15
+    blocks = [[x+ignore_r, x+ignore_r+2*b_half_r, y+b_half_r, y-b_half_r],  # east
+              [x+ignore_r, x+ignore_r+2*b_half_r, y+ignore_r+2*b_half_r, y+ignore_r],  # north east
+              [x-b_half_r, x+b_half_r, y+ignore_r+2*b_half_r, y+ignore_r],  # north
+              [x-ignore_r-2*b_half_r, x-ignore_r, y+ignore_r+2*b_half_r, y+ignore_r],  # north west
+              [x-ignore_r-2*b_half_r, x-ignore_r, y+b_half_r, y-b_half_r],  # west
+              [x-ignore_r-2*b_half_r, x-ignore_r, y-ignore_r, y-ignore_r-2*b_half_r],  # south west
+              [x-b_half_r, x+b_half_r, y-ignore_r, y-ignore_r-2*b_half_r],  # south
+              [x+ignore_r, x+ignore_r+2*b_half_r, y-ignore_r, y-ignore_r-2*b_half_r]]  # south ease
+
+    blocks_occupation = calc_block_occupancy(room, blocks)
+    best_directions = np.where(blocks_occupation == blocks_occupation.min())
+    new_direction = random.choice(best_directions[0])
+
+    return 0
+
+
+def calc_block_occupancy(cur_room, blocks):
+    blocks_occupation = np.zeros(8)
+    for dancer in cur_room.dancers:
+        x, y = dancer.location_x, dancer.location_y
+        for n, block in enumerate(blocks):
+            if (x > block[0]) and (x < block[1]) and (y < block[2]) and (y > block[3]):
+                blocks_occupation[n] += 1
+    return blocks_occupation
+
+
+def find_nn_dancers(location_x, location_y, room):
+    all_locations = np.array([[dancer.location_x, dancer.location_y] for dancer in room.dancers])
+    relative_locations = all_locations - np.array([location_x, location_y])
+    dists = (relative_locations[:, 0]**2 + relative_locations[:, 1]**2)**0.5
+    directions = np.arctan(relative_locations[:, 1]/relative_locations[:, 0])
+    return 0
+
+
+class Room:
+    # object of all dancers and room
+    def __init__(self, n_dancers, width, height):
+        self.width = width
+        self.height = height
+        self.n_dancers = n_dancers
+        self.dancers = []
+        self.create_dancers()
+
+    def create_dancers(self):
+            for n in range(self.n_dancers):
+                self.dancers.append(Dancer(self.width, self.height))
+
+
+class Dancer:
+    # dancer in room
+    def __init__(self, width, height):
+        self.location_x = random.uniform(0, width)
+        self.location_y = random.uniform(0, height)
+        self.speed = 1
+        self.direction = random.uniform(0, 2 * np.pi)
+
+    def update_speed(self):
+        global room
+        global noise
+        new_theta = calc_new_theta(room, self)
+        self.direction = new_theta
+
+
+noise = 1
+room = Room(n_dancers=50, width=100, height=100)
